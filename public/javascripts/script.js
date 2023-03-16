88e7fbe53ef5c60ee7e77115e1e5e77f65e412a7
Anandhu-2002 <anandhupsm2002@gmail.com>
2023-03-16 15:25:17 +0530

    add some style

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,30 +1,6 @@
-// const toggleThemeBtn = document.querySelector('.header__theme-button');
-// // ===================================
-// // DARK/LIGHT THEME
-// // Set initial theme from LocalStorage
-// console.log(toggleThemeBtn);
-// document.onload = setInitialTheme(localStorage.getItem('theme'));
-// function setInitialTheme(themeKey) {
-//   if (themeKey === 'dark') {
-//     document.documentElement.classList.add('darkTheme');
-//   } else {
-//     document.documentElement.classList.remove('darkTheme');
-//   }
-// }
 
-// // Toggle theme button
-// toggleThemeBtn.addEventListener('click', () => {
-//   // Toggle root class
-//   document.documentElement.classList.toggle('darkTheme');
-
-//   // Saving current theme on LocalStorage
-//   if (document.documentElement.classList.contains('darkTheme')) {
-//     localStorage.setItem('theme', 'dark');
-//   } else {
-//     localStorage.setItem('theme', 'light');
-//   }
-// });
 var body=document.querySelector('.body');
+themeChanger=document.querySelector('.theme-change');
 document.onload = setInitialTheme(localStorage.getItem('theme'));
 function setInitialTheme(themeKey) {
    
@@ -34,7 +10,7 @@ function setInitialTheme(themeKey) {
        body.classList.remove("light")
     }
   }
-themeChanger=document.querySelector('.logintheme-btn');
+
 
 themeChanger.addEventListener('click', () =>{
     body.classList.toggle('light');
